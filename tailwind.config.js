76094a5846d7c32187be50342ae21577222c0c10
yuvanbruh <141669130+yuvanbruh@users.noreply.github.com>
2024-10-10 01:19:38 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/img/hero-pattern.svg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    },
+  },
+  plugins: [],
+}
+
+
